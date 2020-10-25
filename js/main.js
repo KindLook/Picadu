fe114318cfbf51de57628601cec10d6596dc8537
provi11 <provi11@yandex.ru>
2020-10-25 14:52:43 +0300

    day3_add: отмена стандартного поведения при клике

--- js/main.js
+++ js/main.js
@@ -1,7 +1,10 @@
+//переменная  - кнопка меню
 let menuToggle = document.querySelector('#menu-toggle');
+//переменная - положим меню
 let menu = document.querySelector('.sidebar')
 
+//отслеживаем клик и запуск функции
 menuToggle.addEventListener('click', function (event) {
-    event.preventDefault();
+    event.preventDefault();   //отменяет стандартный клик - поднимает страницу к началу
     menu.classList.toggle('visible');
 })
